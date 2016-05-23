0846cc1fdc984290b2941013672cb8ea225594f4
3droberto <3droberto@gmail.com>
2016-05-24 01:23:19 +0200

    recurrent calculating of childs data&weight when setAfferentData & visual update of new neuron output datas

--- SCEJS/Prefabs/Graph/graphUtil.js
+++ SCEJS/Prefabs/Graph/graphUtil.js
@@ -18,9 +18,10 @@ var adjMatrix_ForceLayout_GLSLFunctionString = function(geometryLength) {
         'float acumAtraction;'+
         'vec3 repulsion;'+
         'float collisionExists;'+
+        'float netProc;'+
     '};'+
 
-    'CalculationResponse calculate(vec4 it, vec2 xGeom_oppo, vec3 currentPos, vec3 currentDir, vec3 atraction, float acumAtraction, vec3 repulsion) {'+
+    'CalculationResponse calculate(vec4 it, vec2 xGeom_oppo, vec3 currentPos, vec3 currentDir, vec3 atraction, float acumAtraction, vec3 repulsion, float netProc) {'+
         'float radius = 4.0;\n'+
         'float collisionExists = 0.0;\n'+
 
@@ -31,23 +32,26 @@ var adjMatrix_ForceLayout_GLSLFunctionString = function(geometryLength) {
         'vec3 dirToBN = normalize(dirToB);\n'+
         'float dist = distance(currentPosB, currentPos);\n'+ // near=0.0 ; far=1.0
 
+
+        'float childNetworkWaitData = dataB[xGeom_oppo].z;\n'+ // networkWaitData != -2.0 ??
+
         'if(enableForceLayoutCollision == 1.0 && dist < radius) {'+
             'collisionExists = 1.0;'+
             'atraction = sphericalColl(currentDir, currentDirB, dirToBN);'+
         '} else {'+
-            'if(it.x > 0.0) {'+
+            'if(it.x > 0.0) {'+ // connection exists
                 'float bornDateOpposite = dataB[xGeom_oppo].x;'+
                 'float dieDateOpposite = dataB[xGeom_oppo].y;'+
 
-                'int mak = 1;'+
+                'int pass = 1;'+
                 'if(dieDateOpposite != 0.0) {'+
                     'if(currentTimestamp < bornDateOpposite || currentTimestamp > dieDateOpposite) {'+
-                        'mak = 0;'+
+                        'pass = 0;'+
                     '} else {'+
                         // nodes exists, now check link
                         'if(it.y != 0.0) {'+
                             'if(currentTimestamp < it.x || currentTimestamp > it.y) {'+
-                                'mak = 0;'+
+                                'pass = 0;'+
                             '}'+
                         '}'+
                     '}'+
@@ -55,18 +59,21 @@ var adjMatrix_ForceLayout_GLSLFunctionString = function(geometryLength) {
                     // now check link
                     'if(it.y != 0.0) {'+
                         'if(currentTimestamp < it.x || currentTimestamp > it.y) {'+
-                            'mak = 0;'+
+                            'pass = 0;'+
                         '}'+
                     '}'+
                 '}'+
 
-                'if(mak == 1) {'+
+                'if(pass == 1) {'+
                     'if(it.w == 1.0) {'+
                         'atraction += dirToBN*dist*0.5*it.z;\n'+
                         'atraction += dirToBN*-10.0;\n'+
                     '} else {'+
                         'atraction += dirToBN*dist*0.5*it.z;\n'+
                         'atraction += dirToBN*-10.0;\n'+
+
+                        'if(childNetworkWaitData > -2.0) '+
+                            'netProc += (childNetworkWaitData*it.z);'+ // data*weight
                     '}'+
 
                     'acumAtraction += 1.0;\n'+
@@ -76,156 +83,14 @@ var adjMatrix_ForceLayout_GLSLFunctionString = function(geometryLength) {
                     'repulsion += dirToBN*-(10.0);\n'+
             '}'+
         '}'+
-        'return CalculationResponse(atraction, acumAtraction, repulsion, collisionExists);'+
+        'return CalculationResponse(atraction, acumAtraction, repulsion, collisionExists, netProc);'+
     '}'+
-    'vec4 idAdjMatrix_ForceLayout(float nodeId, vec3 currentPos, vec3 currentDir, float numOfConnections, float currentTimestamp, float bornDate, float dieDate) {\n'+
-        // INIT VARS
-        'vec3 atraction = vec3(0.0, 0.0, 0.0);'+
-        'float acumAtraction = 1.0;'+
-        'vec3 repulsion = vec3(0.0, 0.0, 0.0);'+
-
-        'float collisionExists = 0.0;\n'+
-        'vec3 force = vec3(0.0, 0.0, 0.0);\n'+
-        // END INIT VARS
-
-        'float num;float rowAdjMat;float colAdjMat;float initCol;float initRow;'+
-
-        'num = currentAdjMatrix/numberOfColumns;\n'+
-        'rowAdjMat = floor(num);\n'+
-        'colAdjMat = float(int( fract(num)*numberOfColumns ));\n'+
-        'initCol = colAdjMat*widthAdjMatrix;\n'+
-        'initRow = rowAdjMat*widthAdjMatrix;\n'+
-
-        'if(nodeId >= initCol && nodeId < (initCol+widthAdjMatrix)) {\n'+
-
-            'for(int n=0; n < 4096; n++) {\n'+
-                'float idb = float(n)+initRow;\n'+
-                'if(idb >= nodesCount) break;\n'+
-                'if(idb != nodeId) {'+
-                    'vec2 xAdjMat = get_global_id(vec2(nodeId-initCol, float(n)), widthAdjMatrix);'+
-                    'vec4 it = adjacencyMatrix[xAdjMat];\n'+
-
-                    // RELATION FOUND
-                    'vec2 xGeom_oppo = get_global_id(idb, uBufferWidth, '+geometryLength.toFixed(1)+');\n'+
-
-                    'CalculationResponse calcResponse = calculate(it, xGeom_oppo, currentPos, currentDir, atraction, acumAtraction, repulsion);'+
-                    'atraction = calcResponse.atraction;'+
-                    'acumAtraction = calcResponse.acumAtraction;'+
-                    'repulsion = calcResponse.repulsion;'+
-
-                    'if(calcResponse.collisionExists == 1.0) {'+
-                        'collisionExists = 1.0;'+
-                        'force = calcResponse.atraction;'+
-                        'break;'+
-                    '}'+
-
-                    'if(dieDate != 0.0) {'+
-                        'if(currentTimestamp < bornDate || currentTimestamp > dieDate) {'+
-                            'force = vec3(0.0, 0.0, 0.0);'+
-                            'break;'+
-                        '}'+
-                    '}'+
-                    // END RELATION FOUND
-                '}'+
-            '}'+
-            // SUMMATION
-            'if(collisionExists == 0.0) {'+
-                'if(enableForceLayoutRepulsion == 1.0) {'+
-                    'vec3 cA = atraction/acumAtraction;'+
-                    'force += cA;'+
-
-                    'vec3 cR = repulsion/(widthAdjMatrix-acumAtraction);'+
-                    'force += cR*sqrt( max(0.0, 1.0-length(cA)) );'+
-                '} else {'+
-                    'vec3 cA = atraction/acumAtraction;'+
-                    'force += cA;'+
-                '}'+
-            '}'+
-            // END SUMMATION
-        '}'+
-
-        'return vec4(force, collisionExists);'+
-    '}';
-
-    return str;
-};
-
-var adjMatrix_NeuronalNetwork_Efference_GLSLFunctionString = function(geometryLength) {
-    var str = ''+
-    'vec3 sphericalColl(vec3 currentDir, vec3 currentDirB, vec3 dirToBN) {'+
-        'vec3 currentDirN = normalize(currentDir);'+
-        'float pPoint = abs(dot(currentDirN, dirToBN));'+
-        'vec3 reflectV = reflect(currentDirN*-1.0, dirToBN);'+
-
-        'vec3 currentDirBN = normalize(currentDirB);'+
-        'float pPointB = abs(dot(currentDirBN, dirToBN));'+
-
-        'vec3 repulsionForce = (reflectV*-1.0)* (((1.0-pPoint)*length(currentDir))+((pPointB)*length(currentDirB)));\n'+
-
-        'return (repulsionForce.x > 0.0 && repulsionForce.y > 0.0 && repulsionForce.z > 0.0) ? repulsionForce : dirToBN*-0.1;'+
-    "}"+
-
-    'struct CalculationResponse {'+
-        'vec3 atraction;'+
-        'float acumAtraction;'+
-        'vec3 repulsion;'+
+    'struct idAdjMatrixResponse {'+
+        'vec3 force;'+
         'float collisionExists;'+
+        'float netProcData;'+
     '};'+
-
-    'CalculationResponse calculate(vec4 it, vec2 xGeom_oppo, vec3 currentPos, vec3 currentDir, vec3 atraction, float acumAtraction, vec3 repulsion) {'+
-        'float radius = 4.0;\n'+
-        'float collisionExists = 0.0;\n'+
-
-        'vec3 currentPosB = posXYZW[xGeom_oppo].xyz;\n'+
-        'vec3 currentDirB = dir[xGeom_oppo].xyz;\n'+
-
-        'vec3 dirToB = (currentPosB-currentPos);\n'+
-        'vec3 dirToBN = normalize(dirToB);\n'+
-        'float dist = distance(currentPosB, currentPos);\n'+ // near=0.0 ; far=1.0
-
-        'if(enableForceLayoutCollision == 1.0 && dist < radius) {'+
-            'collisionExists = 1.0;'+
-            'atraction = sphericalColl(currentDir, currentDirB, dirToBN);'+
-        '} else {'+
-            'if(it.x > 0.0) {'+
-                'float bornDateOpposite = dataB[xGeom_oppo].x;'+
-                'float dieDateOpposite = dataB[xGeom_oppo].y;'+
-
-                'int mak = 1;'+
-                'if(dieDateOpposite != 0.0) {'+
-                    'if(currentTimestamp < bornDateOpposite || currentTimestamp > dieDateOpposite) {'+
-                        'mak = 0;'+
-                    '} else {'+
-                        // nodes exists, now check link
-                        'if(it.y != 0.0) {'+
-                            'if(currentTimestamp < it.x || currentTimestamp > it.y) {'+
-                                'mak = 0;'+
-                            '}'+
-                        '}'+
-                    '}'+
-                '} else {'+
-                    // now check link
-                    'if(it.y != 0.0) {'+
-                        'if(currentTimestamp < it.x || currentTimestamp > it.y) {'+
-                            'mak = 0;'+
-                        '}'+
-                    '}'+
-                '}'+
-
-                'if(mak == 1) {'+
-                    'atraction += dirToBN*dist*0.5*it.z;\n'+
-                    'atraction += dirToBN*-10.0;\n'+
-
-                    'acumAtraction += 1.0;\n'+
-                '}'+
-            '} else {'+
-                'if(enableForceLayoutRepulsion == 1.0) \n'+
-                    'repulsion += dirToBN*-(10.0);\n'+
-            '}'+
-        '}'+
-        'return CalculationResponse(atraction, acumAtraction, repulsion, collisionExists);'+
-    '}'+
-    'vec4 idAdjMatrix_NeuronalNetwork_Efference(float nodeId, vec3 currentPos, vec3 currentDir, float numOfConnections, float currentTimestamp, float bornDate, float dieDate) {\n'+
+    'idAdjMatrixResponse idAdjMatrix_ForceLayout(float nodeId, vec3 currentPos, vec3 currentDir, float numOfConnections, float currentTimestamp, float bornDate, float dieDate) {\n'+
         // INIT VARS
         'vec3 atraction = vec3(0.0, 0.0, 0.0);'+
         'float acumAtraction = 1.0;'+
@@ -233,6 +98,8 @@ var adjMatrix_NeuronalNetwork_Efference_GLSLFunctionString = function(geometryLe
 
         'float collisionExists = 0.0;\n'+
         'vec3 force = vec3(0.0, 0.0, 0.0);\n'+
+
+        'float netProc = 0.0;'+
         // END INIT VARS
 
         'float num;float rowAdjMat;float colAdjMat;float initCol;float initRow;'+
@@ -255,10 +122,11 @@ var adjMatrix_NeuronalNetwork_Efference_GLSLFunctionString = function(geometryLe
                     // RELATION FOUND
                     'vec2 xGeom_oppo = get_global_id(idb, uBufferWidth, '+geometryLength.toFixed(1)+');\n'+
 
-                    'CalculationResponse calcResponse = calculate(it, xGeom_oppo, currentPos, currentDir, atraction, acumAtraction, repulsion);'+
+                    'CalculationResponse calcResponse = calculate(it, xGeom_oppo, currentPos, currentDir, atraction, acumAtraction, repulsion, netProc);'+
                     'atraction = calcResponse.atraction;'+
                     'acumAtraction = calcResponse.acumAtraction;'+
                     'repulsion = calcResponse.repulsion;'+
+                    'netProc = calcResponse.netProc;'+
 
                     'if(calcResponse.collisionExists == 1.0) {'+
                         'collisionExists = 1.0;'+
@@ -291,7 +159,7 @@ var adjMatrix_NeuronalNetwork_Efference_GLSLFunctionString = function(geometryLe
             // END SUMMATION
         '}'+
 
-        'return vec4(force, collisionExists);'+
+        'return idAdjMatrixResponse(vec3(force), collisionExists, netProc);'+
     '}';
 
     return str;
